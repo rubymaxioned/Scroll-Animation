85428cf3a5f70b66bc9ed3ca63ae0720e6511c07
rubymaxioned <rubym@axioned.com>
2022-11-13 20:09:56 +0530

    changes

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,6 @@ const boxes = document.querySelectorAll(".box");
 window.addEventListener('scroll', myFunction);
 function myFunction() {
     const triggerBottom = window.innerHeight / 2;
-    console.log("trigger bottom is" + triggerBottom);
     boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top;
         if (boxTop < triggerBottom) {
