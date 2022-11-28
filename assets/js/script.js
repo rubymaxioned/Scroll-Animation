99a1539f880521ab514ef77b2462cfa18a40513c
rubymaxioned <axioned0482022>
2022-11-28 19:24:33 +0530

    reduced height

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const boxes = document.querySelectorAll(".box");
 window.addEventListener('scroll', myFunction);
 function myFunction() {
-    const triggerBottom = window.innerHeight / 2;
+    const triggerBottom = (window.innerHeight * 3) / 4;
     boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top;
         if (boxTop < triggerBottom) {
